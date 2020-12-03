b4cff6cee2c538b69ea1a60f0ca4833ce66a564b
Andrey Zinoviev <dronis_dronis@mail.ru>
2020-12-03 15:55:22 +0300

    добавить анимацию

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,16 @@
 const headline = document.querySelector('.main__headline');
 const header = document.querySelector('.header');
 
-document.addEventListener('scroll', function() {
-    if(pageYOffset >= 100) {
-        header.classList.add('header_background-added');
-    } else if (pageYOffset <= headline.offsetTop) {
+document.addEventListener('scroll', (evt) => {
+    if (pageYOffset >= 100) {
+        header.classList.add('header_background-added')
+    } else {
         header.classList.remove('header_background-added');
     }
+    
 })
+
+headline.classList.add('main__headline_active');
+setInterval(() => {
+    headline.classList.add('main__headline_fixed')
+}, 2000)    
