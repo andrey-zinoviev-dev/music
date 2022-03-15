1da04229afa5235e2eee2f34e7be357296b12b5a
Andrey Zinoviev <dronis_dronis@mail.ru>
2022-03-15 07:04:16 +0300

    made order page layout

--- scripts/orderPage.js
+++ scripts/orderPage.js
@@ -1,1 +1,13 @@
-// console.log(goodsToAddToCart);
+//selectors
+const orderSection = document.querySelector('.order');
+const orderSubtotalPriceSpan = orderSection.querySelector('.order__details-price-subtotal');
+const orderDeliveryPriceSpan = orderSection.querySelector('.order__details-price-delivery');
+const orderTotalPriceSpan = orderSection.querySelector('.order__details-price-total');
+
+//functions
+function sumSelectorValues(value1, value2) {
+  return +value1 + +value2;
+};
+
+const finalSum = sumSelectorValues(orderSubtotalPriceSpan.textContent, orderDeliveryPriceSpan.textContent);
+orderTotalPriceSpan.textContent = finalSum;
